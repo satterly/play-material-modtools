89bf07f1799d743241d58d254981454eb82cba0a
Nick Satterly <nick.satterly@guardian.co.uk>
2015-07-06 02:17:52 +0100

    Add dates

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,35 +1,12 @@
 package controllers
 
-import javax.inject.Inject
-
 import models.{CommentTable, Comment}
 import play.api.Play
 import play.api.db.slick.{HasDatabaseConfig, DatabaseConfigProvider}
 import play.api.mvc._
 import play.api.libs.json._
-import slick.driver
 import slick.driver.JdbcProfile
-import slick.lifted._
 
-//import scala.concurrent.Future
-//
-////class Application @Inject()(dbConfigProvider: DatabaseConfigProvider) extends Controller {
-//class Application extends Controller with CommentTable {
-//  //val dbConfig = dbConfigProvider.get[JdbcProfile]
-//  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
-//  import driver.api._
-//
-//  def index = Action.async { implicit request =>
-//
-////    val Comments = TableQuery[Comments]
-////
-////    val commentsResult: Future[Seq[Comment]] = dbConfig.db.run(Comments.result)
-////    commentsResult.map(comments => Ok(Json.toJson(comments)))
-//
-//    db.run(Comments.result).map(res => Ok(Json.toJson(res.toList)))
-//  }
-//
-//}
 
 class Application extends Controller with CommentTable with HasDatabaseConfig[JdbcProfile]{
 
@@ -38,8 +15,7 @@ class Application extends Controller with CommentTable with HasDatabaseConfig[Jd
   val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
   import driver.api._
 
-  //create an instance of the table
-  val Comments = TableQuery[Comments] //see a way to architect your app in the computers-database sample
+  val Comments = TableQuery[Comments]
 
   def index = Action.async {
     db.run(Comments.result).map(res => Ok(Json.obj("data" -> res.toList)))
--- app/models/Comment.scala
+++ app/models/Comment.scala
@@ -1,21 +1,29 @@
 package models
 
 import java.sql.Timestamp
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
 
 import slick.driver.JdbcProfile
 import slick.driver.PostgresDriver.api._
 import slick.lifted._
+import play.api.libs.json.Json._
 import play.api.libs.json._
 
 case class Comment(
   id: Option[Long],
   body: String,
- // created_on: Timestamp,
- // last_updated: Timestamp,
+  created_on: Timestamp,
+  last_updated: Timestamp,
   status: String)
 
 object Comment {
 
+  implicit val dateTimeWrites = new Writes[Timestamp] {
+    def writes(t: Timestamp): JsValue = JsString(ISODateTimeFormat.dateTime.print(
+      new DateTime(t))
+    )
+  }
   implicit val commentWrites = Json.writes[Comment]
 }
 
@@ -28,28 +36,11 @@ trait CommentTable {
 
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def body = column[String]("body", O.SqlType("TEXT"))
- //   def created_on = column[Timestamp]("created_on")
- //   def last_updated = column[Timestamp]("last_updated")
+    def created_on = column[Timestamp]("created_on")
+    def last_updated = column[Timestamp]("last_updated")
     def status = column[String]("status")
 
-    //def * = (id.?, body, created_on, last_updated, status) <>((Comment.apply _).tupled, Comment.unapply _)
-    def * = (id.?, body, status) <>((Comment.apply _).tupled, Comment.unapply _)
+    def * = (id.?, body, created_on, last_updated, status) <>((Comment.apply _).tupled, Comment.unapply _)
   }
 
 }
-
-//object Comments {
-
-//  val Comments = TableQuery[Comments]
-
-//  def create(comment: Comment)(implicit s:Session) = {
-//    comments returning comments.map(_.id) += comment
-//  }
-
-//  def list: Future[Seq[Comment]] = {
-//    val commentList = comments.result
-//
-//    db.run(commentList)
-//  }
-
-//}
